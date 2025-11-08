3ecd60acaa294f0297f27454ea61ebc9ee0cf26b
rachadelrhilani <rachdos76@gmail.com>
2025-11-08 20:02:51 +0100

    fix footer

--- js/detail-plat.js
+++ js/detail-plat.js
@@ -1,9 +1,9 @@
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
-const header = document.querySelector("header");
 menuBtn.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
+const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     if (window.scrollY > 0) {
         header.classList.add("fixed", "top-0", "left-0", "right-0", "shadow-md");
