208faab26edf234a7dc41c0a1760a972dfe4e449
rachadelrhilani <rachdos76@gmail.com>
2025-11-08 21:54:35 +0100

    progression dans la page

--- js/detail-plat.js
+++ js/detail-plat.js
@@ -6,10 +6,11 @@ menuBtn.addEventListener('click', () => {
 const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     if (window.scrollY > 0) {
-        header.classList.add("fixed", "top-0", "left-0", "right-0", "shadow-md");
-        header.classList.remove("-translate-y-full");
+        header.classList.add("fixed", "top-0", "left-0", "right-0", "shadow-md", "translate-y-0");
+        header.classList.add("transform","-translate-y-full","transition-transform","duration-300","ease-in-out");
+        mobileMenu.classList.add("fixed", "top-[60px]", "left-0", "right-0", "w-full");
     } else {
-        header.classList.remove("fixed", "top-0", "left-0", "right-0", "shadow-md");
-        header.classList.remove("-translate-y-full");
+        header.classList.remove("fixed", "top-0", "left-0", "right-0", "shadow-md", "translate-y-0");
+        header.classList.remove("transform","-translate-y-full","transition-transform","duration-300","ease-in-out");
     }
 });
