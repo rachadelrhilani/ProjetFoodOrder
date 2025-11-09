29d8642cbe8a8dc7526c199c96b14e3ef295c9fb
rachadelrhilani <rachdos76@gmail.com>
2025-11-09 12:21:32 +0100

    modification dans le header

--- js/detail-plat.js
+++ js/detail-plat.js
@@ -5,7 +5,7 @@ menuBtn.addEventListener('click', () => {
 });
 const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 0) {
+    if (window.scrollY > 10) {
         header.classList.add("fixed", "top-0", "left-0", "right-0", "shadow-md", "translate-y-0");
         header.classList.add("transform","-translate-y-full","transition-transform","duration-300","ease-in-out");
         mobileMenu.classList.add("fixed", "top-[60px]", "left-0", "right-0", "w-full");
