17c046e3951a748d91955c4e7c70dd8189e04117
abdelghafour <achbaniabdelghafour8@gmail.com>
2025-11-09 17:54:57 +0100

    finition de la partie html et css du page acceuill

--- Acceuipage/acceuil.js
+++ Acceuipage/acceuil.js
@@ -1,8 +1,17 @@
 let burger = document.getElementById("burger");
 let navright = document.querySelector(".nav-right");
+let nav = document.querySelector("nav");
 
 burger.addEventListener("click", function () {
     navright.classList.toggle("active");
 });
 
-// fin le burger menu 
+//////////////////////// nav bar (responsive) ///////////////////
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+        nav.classList.add("scrol");
+    } else {
+        nav.classList.remove("scrol");
+    }
+});
