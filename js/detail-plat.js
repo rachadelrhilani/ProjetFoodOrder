9267e9b19564dac228af4edf692a40728cc46282
rachad <rachdos76@gmail.com>
2025-11-10 17:28:17 +0100

    add menu.json

--- js/detail-plat.js
+++ js/detail-plat.js
@@ -7,10 +7,59 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     if (window.scrollY > 10) {
         header.classList.add("fixed", "top-0", "left-0", "right-0", "shadow-md", "translate-y-0");
-        header.classList.add("transform","-translate-y-full","transition-transform","duration-300","ease-in-out");
+        header.classList.add("transform", "-translate-y-full", "transition-transform", "duration-300", "ease-in-out");
         mobileMenu.classList.add("fixed", "top-[60px]", "left-0", "right-0", "w-full");
     } else {
         header.classList.remove("fixed", "top-0", "left-0", "right-0", "shadow-md", "translate-y-0");
-        header.classList.remove("transform","-translate-y-full","transition-transform","duration-300","ease-in-out");
+        header.classList.remove("transform", "-translate-y-full", "transition-transform", "duration-300", "ease-in-out");
     }
 });
+
+fetch("../data/menu.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erreur de chargement du fichier JSON");
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Exemple : récupérer le plat avec l'id 1
+        const plat = data.find(item => item.id === 1);
+        console.log(plat);
+        const container = document.getElementById("plat-content");
+        if (!plat) {
+            container.innerHTML = "<p class='text-center text-red-500'>Plat introuvable.</p>";
+            return;
+        }
+        // Créer le HTML dynamiquement
+        container.innerHTML = `
+      <div>
+        <h3 class="text-2xl font-bold mb-6 text-center">${plat.name}</h3>
+        <img src="${plat.image}" alt="${plat.name}" 
+  class=" shadow-md mx-auto mb-6 w-full max-w-2xl h-[450px] object-cover detail" />
+      </div>
+
+      <p class="text-gray-700 text-left leading-relaxed mb-6">
+        ${plat.description}
+      </p>
+
+      <div>
+        <div>
+          <strong class="text-left">Ingrédients :</strong>
+          <ul class="text-left text-yellow-500 mb-6">
+            ${plat.ingredients.map(ing => `<li>${ing}</li>`).join('')}
+          </ul>
+        </div>
+      </div>
+
+      <div class="flex justify-center">
+        <button class="bg-yellow-500 flex justify-center hover:bg-yellow-400 text-white ml-6 px-4 py-2 font-semibold space-x-2">
+          <span>Ajouter au Panier</span>
+          <img src="../images/Shopping cart.png" alt="Shopping cart" class="w-5 h-5" />
+        </button>
+      </div>
+    `;
+    })
+    .catch(error => {
+        console.error("Erreur :", error);
+    });
