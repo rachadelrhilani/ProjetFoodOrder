2f7352c6a165d7f9a6723cdabf0483f7883efb7f
ZiyadCh <chetoziyad@gmail.com>
2025-11-11 09:43:08 +0100

    paimobile

---
+++ js/panier.js
@@ -0,0 +1,7 @@
+const pay = document.querySelector(".paiment ");
+const panier = document.querySelector(".panier");
+
+function toPay() {
+  pay.style.display = "flex";
+  panier.style.display = "none";
+}
