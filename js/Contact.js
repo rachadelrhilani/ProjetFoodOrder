1cd6098b565f7137ef796ce86ac2880f19f2a0c5
MOUAINE <fatihamouaine1@gmail.com>
2025-11-13 15:24:30 +0100

    add partie faq

--- js/Contact.js
+++ js/Contact.js
@@ -1,13 +1,13 @@
 const form = document.querySelector('form');
 const nom = document.getElementById('name');
 const email = document.getElementById('email');
-const tele= document.getElementById('phone');
-const message= document.getElementById('message');
+const tele = document.getElementById('phone');
+const message = document.getElementById('message');
 
-const nomV = /^[a-zA-Z\s]{3,30}$/; 
-const emailV= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const phoneV= /^[0-9\s\+\-]{9,10}$/;
-const messageV= /^.{10,500}$/;
+const nomV = /^[a-zA-Z\s]{3,30}$/;
+const emailV = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phoneV = /^[0-9\s\+\-]{9,10}$/;
+const messageV = /^.{10,500}$/;
 function affichage(input, message) {
     const err = input.parentElement.querySelector('.error-text');
     if (err) {
@@ -20,15 +20,15 @@ function affichage(input, message) {
     input.style.borderColor = 'red';
 }
 function verif(input) {
-    const messE= input.parentElement.querySelector('.error-text');
+    const messE = input.parentElement.querySelector('.error-text');
     if (messE) {
         messE.remove();
     }
-    input.style.borderColor='';
+    input.style.borderColor = '';
 }
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();  
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
     let valid = true;
     verif(nom);
     verif(email);
@@ -38,7 +38,7 @@ form.addEventListener('submit', function(e) {
         affichage(nom, 'Le nom doit contenir entre 3 et 30 lettres');
         valid = false;
     }
-    
+
     if (!emailV.test(email.value)) {
         affichage(email, 'Attention votre email invalide');
         valid = false;
@@ -53,8 +53,21 @@ form.addEventListener('submit', function(e) {
     }
     if (valid) {
         alert('Votre message est envoye avec succes!');
-        form.reset(); 
+        form.reset();
     }
 });
+const questions = document.querySelectorAll('.qst');
 
-
+questions.forEach(button => {
+    button.addEventListener('click', () => {
+        const reponse = button.nextElementSibling;
+        const icone = button.querySelector('.icon');
+        if (reponse.classList.contains('hidden')) {
+            reponse.classList.remove('hidden');
+            icone.textContent = '–';
+        } else {
+            reponse.classList.add('hidden');
+            icone.textContent = '+';
+        }
+    });
+});
